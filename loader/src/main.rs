315805d44fe7f26f649473d8264798edf0e60375
esposm03 <36164633+esposm03@users.noreply.github.com>
2024-09-21 16:32:29 +0200

    Basic prototype

---
+++ loader/src/main.rs
@@ -0,0 +1,16 @@
+use core::slice;
+use std::{env::args, ptr::null_mut};
+
+use vsbf::parse_file;
+
+fn main() {
+    let buf = std::fs::read(args().nth(1).unwrap()).unwrap();
+    let (data, file) = parse_file(&buf).unwrap();
+
+    let map = unsafe {
+        let map = libc::mmap(null_mut(), 0x1000, 7, 0, 0, 0).cast::<u8>();
+        slice::from_raw_parts_mut(map, 0x1000)
+    };
+
+    println!("data: {:x?}", map);
+}
